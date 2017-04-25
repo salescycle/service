0535d0c6e6eeff07e688ac1e6856dd34e55e34d0
ram4975 <ananth.r.ana@gmail.com>
2017-04-25 06:24:32 -0500

    added mysql db connectivity

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var mysql      = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'us-cdbr-azure-southcentral-f.cloudapp.net',
+  user     : 'bf2153f494542e',
+  password : '2df8ca57',
+  database : 'acsm_e2f8e2c1b156bb0'
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  connection.connect();
+  connection.query('SELECT * from users', function(err, rows, fields) {
+    if (!err)
+      res.send(rows);
+    else
+      res.send('Error while performing Query.');
+  });  
 });
 
 module.exports = router;
