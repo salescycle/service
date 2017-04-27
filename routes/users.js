3e4ddb20ae4d480dcc46c281b7821ef946cbcc11
Kurt Steinpreis <kurt.steinpreis@gmail.com>
2017-04-27 13:13:58 -0500

    removed all my crap

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,22 @@
 var express = require('express');
 var router = express.Router();
-var mysql      = require('mysql');
+var mysql = require('mysql');
+var pool = require('./common/connection-pool')
 
-var connection = mysql.createConnection({
-  host     : 'us-cdbr-azure-southcentral-f.cloudapp.net',
-  user     : 'bf2153f494542e',
-  password : '2df8ca57',
-  database : 'acsm_e2f8e2c1b156bb0'
-});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  connection.connect();
-  connection.query('SELECT * from users', function(err, rows, fields) {
-    if (!err)
-      res.send(rows);
-    else
-      res.send('Error while performing Query.');
-  });  
-  connection.end();
+    pool.getConnection(function(err, connection) {
+        // Use the connection
+        connection.query('SELECT * FROM users', function(error, results, fields) {
+            // And done with the connection.
+            res.send(results);
+            connection.release();
+            // Handle error after the release.
+            if (error) throw error;
+            // Don't use the connection here, it has been returned to the pool.
+        });
+    });
 });
 
 module.exports = router;
